eac6881704e17913409c427869c267f405484030
bhaskardavuluri <bhaskardavuluri9949@gmail.com>
2025-07-01 11:15:46 +0530

    All

---
+++ Class-1 Basics/three.js
@@ -0,0 +1,10 @@
+import os from 'os';//ES6 (to enable es6 features in node js we need to install node modules by using cmd npm init -y)
+//const os=require('os')//ES5 import and export methods ..
+console.log(os.homedir());
+console.log(os.type());
+console.log(os.arch());
+console.log(os.release());
+//console.log(os.cpus());
+ console.log(os.totalmem());
+ console.log(os.freemem());
+console.log(os.platform());
---
+++ Class-1 Basics/two.js
@@ -0,0 +1,7 @@
+//set timeout ()- is a javascript function.
+//executing provided function after certain time interval.
+setTimeout(()=>{console.log("request-1");},4000);
+setTimeout(()=>{console.log("request-2");},2000);
+setTimeout(()=>{console.log("request-3");},3000);
+setTimeout(()=>{console.log("request-4");},1000);
+//callback-callback is nothing but passing function as a argument..
